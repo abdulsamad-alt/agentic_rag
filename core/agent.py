b1ab4588efb76ea6b02984e4a746edfd6db6396e
Abdul Samad <abdulsamad552810@gmail.com>
2026-05-06 02:15:30 +0500

    fix index build before agent load

--- core/agent.py
+++ core/agent.py
@@ -19,7 +19,7 @@ class AgentSystem:
 
         # LLM
         genai.configure(api_key=GEMINI_API_KEY)
-        self.llm = genai.GenerativeModel("gemini-2.5-flash-lite")  # or "gemini-1.5-flash"
+        self.llm = genai.GenerativeModel("gemini-2.5-flash")  # or "gemini-1.5-flash"
 
         # Memory
         self.chat_history = []
